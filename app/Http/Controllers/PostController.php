9f21a059b6b9fc0f9acf37c7f9e7e7fe93b26762
Jaroslav Klimcik <jklimcik@slido.com>
2023-07-11 12:10:29 +0200

    fourth part

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\User;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -23,7 +25,9 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('posts.create');
+        $users = User::all();
+        $categories = Category::all();
+        return view('posts.create', compact('users', 'categories'));
     }
 
     /**
@@ -34,12 +38,16 @@ class PostController extends Controller
         $request->validate([
             'title' => 'required|max:255',
             'content' => 'required',
+            'author' => 'required|exists:users,id',
+            'category' => 'required|exists:categories,id',
         ]);
 
         $post = new Post();
         $post->title = $request->input('title');
         $post->slug = Str::slug($request->input('title'));
         $post->content = $request->input('content');
+        $post->user_id = $request->input('author');
+        $post->category_id = $request->input('category');
         $post->save();
 
         return redirect()->route('posts.index')->with('success', 'Článek byl úspěšně vytvořen.');
